7924cc742d00f119f4b2d1ae16fe2d07369c68b2
milardich <stjepan.milardic1@gmail.com>
2022-10-25 18:24:14 +0200

    Camera zoom in / zoom out

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
@@ -9,19 +10,23 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] bool movingLeft = false;
     [SerializeField] bool movingRight = false;
     [SerializeField] float cameraMoveSpeed = 20f;
-    [SerializeField] int cameraBoundsSize = 5;
+    [SerializeField] int cameraMoveBounds = 5;
+    [SerializeField] float scrollSpeed = 3.0f;
+    private float minCameraHeight = 5.0f;
+    float maxCameraHeight = 25.0f;
 
     void Update()
     {
-        movingLeft = Input.mousePosition.x < cameraBoundsSize;
-        movingDown = Input.mousePosition.y < cameraBoundsSize;
-        movingRight = Input.mousePosition.x > Screen.width - cameraBoundsSize;
-        movingUp = Input.mousePosition.y > Screen.height - cameraBoundsSize;
+        movingLeft = Input.mousePosition.x < cameraMoveBounds;
+        movingDown = Input.mousePosition.y < cameraMoveBounds;
+        movingRight = Input.mousePosition.x > Screen.width - cameraMoveBounds;
+        movingUp = Input.mousePosition.y > Screen.height - cameraMoveBounds;
 
         MoveUp();
         MoveLeft();
         MoveRight();
         MoveDown();
+        Zoom();
     }
 
     public void MoveUp()
@@ -54,5 +59,24 @@ public class CameraMovement : MonoBehaviour
         {
             this.gameObject.transform.Translate(new Vector3(cameraMoveSpeed * Time.deltaTime, 0.0f, 0.0f));
         }
+    }
+
+    public void Zoom()
+    {
+        Debug.Log(Input.mouseScrollDelta.y);
+        Vector3 pos = this.gameObject.transform.position;
+        pos.y -= Input.mouseScrollDelta.y * scrollSpeed;
+        if (pos.y >= minCameraHeight && pos.y <= maxCameraHeight)
+        {
+            this.gameObject.transform.position = pos;
+        }
+        else if (pos.y > maxCameraHeight)
+        {
+            this.gameObject.transform.position = new Vector3(pos.x, maxCameraHeight, pos.z);
+        }
+        else
+        {
+            this.gameObject.transform.position = new Vector3(pos.x, minCameraHeight, pos.z);
+        }   
     }
 }
