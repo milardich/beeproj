774b49f558b01c5427f073a97bdd2809591ce1ac
milardich <stjepan.milardic1@gmail.com>
2022-10-25 15:12:58 +0200

    camera movement jitter fixed by placing movement in Update method instead of FixedUpdate

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] Vector3 mousePosition;
-    [SerializeField] Vector3 cameraPosition;
     [SerializeField] bool movingUp = false;
     [SerializeField] bool movingDown = false;
     [SerializeField] bool movingLeft = false;
@@ -13,66 +11,45 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] float cameraMoveSpeed = 20f;
     [SerializeField] int cameraBoundsSize = 5;
 
-    void Start()
-    {
-        cameraPosition = transform.position;
-    }
-
     void Update()
     {
-        mousePosition = Input.mousePosition;
+        movingLeft = Input.mousePosition.x < cameraBoundsSize;
+        movingDown = Input.mousePosition.y < cameraBoundsSize;
+        movingRight = Input.mousePosition.x > Screen.width - cameraBoundsSize;
+        movingUp = Input.mousePosition.y > Screen.height - cameraBoundsSize;
 
-        if (Input.mousePosition.x < cameraBoundsSize)
-        {
-            movingLeft = true;
-        }
-        else
-        {
-            movingLeft = false;
-        }
-
-        if(Input.mousePosition.y < cameraBoundsSize)
-        {
-            movingDown = true;
-        }
-        else
-        {
-            movingDown = false;
-        }
-
-        if(Input.mousePosition.x > Screen.width - cameraBoundsSize)
-        {
-            movingRight = true;
-        }
-        else
-        {
-            movingRight = false;
-        }
-
-        if(Input.mousePosition.y > Screen.height - cameraBoundsSize)
-        {
-            movingUp = true;
-        }
-        else
-        {
-            movingUp = false;
-        }
+        MoveUp();
+        MoveLeft();
+        MoveRight();
+        MoveDown();
     }
 
-    void FixedUpdate()
+    public void MoveUp()
     {
         if (movingUp)
         {
             this.gameObject.transform.Translate(new Vector3(0.0f, 0.0f, cameraMoveSpeed * Time.deltaTime));
         }
+    }
+
+    public void MoveLeft()
+    {
         if (movingLeft)
         {
-            this.gameObject.transform.Translate(new Vector3(-cameraMoveSpeed * Time.deltaTime, 0.0f , 0.0f));
+            this.gameObject.transform.Translate(new Vector3(-cameraMoveSpeed * Time.deltaTime, 0.0f, 0.0f));
         }
+    }
+
+    public void MoveDown()
+    {
         if (movingDown)
         {
             this.gameObject.transform.Translate(new Vector3(0.0f, 0.0f, -cameraMoveSpeed * Time.deltaTime));
         }
+    }
+
+    public void MoveRight()
+    {
         if (movingRight)
         {
             this.gameObject.transform.Translate(new Vector3(cameraMoveSpeed * Time.deltaTime, 0.0f, 0.0f));
