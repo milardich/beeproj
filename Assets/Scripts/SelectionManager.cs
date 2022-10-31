db0a3e89288cf18c86d30bbd9e2d2dbd9831a87b
milardich <stjepan.milardic1@gmail.com>
2022-10-31 14:56:42 +0100

    Added selection manager class,, implemented click selecting

---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    private InputManager inputManager;
+    private UnitManager unitManager;
+    private int selectableLayerMask;
+
+    void Awake()
+    {
+        selectableLayerMask = LayerMask.GetMask("Selectable");
+    }
+
+    void Start()
+    {
+        inputManager = InputManager.Instance;
+        unitManager = UnitManager.Instance;
+    }
+
+    void Update()
+    {
+        ClickSelect();    
+    }
+
+    
+    /*
+     * Selectable layer mask is used to deselect all units in case of
+     * clicking on objects that are not in that same layer mask
+     */
+    public void ProcessSelection()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity, selectableLayerMask))
+        {
+            ISelectable selectable = hit.collider.GetComponent<ISelectable>();
+            if(selectable != null)
+            {
+                selectable.Select();
+            }
+        }
+        else
+        {
+            unitManager.DeselectAllUnits();
+        }
+    }
+
+    public void ClickSelect()
+    {
+        if (Input.GetMouseButtonDown(inputManager.mouseLeft))
+        {
+            unitManager.DeselectAllUnits();
+            ProcessSelection();
+        }
+    }
+
+    //TODO: drag select
+    //TODO: ctrl click/drag select (holding ctrl and selecting adds units to selectedUnits)
+}
